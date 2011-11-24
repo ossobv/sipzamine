81717e13c02374d4d0afb038b995ca8987db1290
Walter Doekes <walter+github@wjd.nu>
2011-11-24 22:21:46 +0100

    Fix that --maxdur works properly for microseconds. Allow multiple -m matches.

--- contrib/sipcaparseye.py
+++ contrib/sipcaparseye.py
@@ -6,22 +6,13 @@
 from libdata import PcapReader, VerboseTcpdumpReader
 from libprotosip import SipDialogs # must import file so the type is registered
 
+# Matches dialogs and times and looks for certain info (EYE).
 # FIXME: rename this to: sipzamin (sip examine)
 # TODO: the verbosereader should add CR's back.. now that we have the libpcapreader it should be deprecated too..
-# TODO: accept more packet-match-re's to AND together
-
-# Matches dialogs and times and looks for certain info (EYE).
-# Takes tcpdump -vs0 udp and port 5060 as input on stdin.
 
 
-def main(reader, packet_match=None, packet_highlight=None, min_duration=None, max_duration=None):
-    # Use packet_match as packet_highlight if show isn't supplied
-    if not packet_highlight:
-        packet_highlight = packet_match
-
-    # Loop over dialogs, keeping those that match
-    matching_dialogs = []
-    for dialog in SipDialogs(reader):
+def dialog_filter(reader, packet_matches=None, min_duration=None, max_duration=None):
+    for dialog in reader:
         # Filter against duration
         if min_duration or max_duration:
             duration = dialog[-1].datetime - dialog[0].datetime
@@ -31,38 +22,51 @@ def main(reader, packet_match=None, packet_highlight=None, min_duration=None, ma
                 continue
         
         # Match dialogs by packet
-        if packet_match:
-            for packet in dialog:
-#                # HACKS!
-#                if not dialog[0].from_[0].startswith('62'):
-#                    continue
-                if packet.search(packet_match):
-                    matching_dialogs.append(dialog)
+        if packet_matches:
+            matched = True
+            for packet_match in packet_matches:
+                # All matches must match
+                for packet in dialog:
+                    if packet.search(packet_match):
+                        break
+                else:
+                    matched = False
                     break
-        else:
-            matching_dialogs.append(dialog)
 
-    # Order dialogs by begin-time
+            if not matched:
+                continue
+                    
+        yield dialog
+    
+def print_dialog(dialog, packet_highlight=None):
+    print '[', dialog[0].callid, ']'
+    for packet in dialog:
+        highlight = ''
+        if packet_highlight:
+            found_here = packet.search(packet_highlight)
+            if found_here:
+                if not found_here.groups():
+                    highlight = '<--'
+                else:
+                    highlight = '<-- %s' % (found_here.groups()[0],)
+            
+        print '%s %s:%d > %s:%d %s %s %s' % (
+            packet.datetime, packet.from_[0], packet.from_[1], packet.to[0], packet.to[1],
+            packet.cseq[0], packet.method_and_status, highlight
+        )
+    print
+
+def main(reader, packet_matches=None, packet_highlight=None, min_duration=None, max_duration=None):
+    # Filter the dialogs
+    matching_dialogs = []
+    for dialog in dialog_filter(SipDialogs(reader), packet_matches, min_duration, max_duration):
+        print_dialog(dialog, packet_highlight)
+        #matching_dialogs.append(dialog)
+
+    # Order dialogs by begin-time and first then print them
     matching_dialogs.sort(key=lambda x: x[0].datetime)
-
-    # Print the matching dialogs and packet_highlight matches
     for dialog in matching_dialogs:
-        print '[', dialog[0].callid, ']'
-        for packet in dialog:
-            highlight = ''
-            if packet_highlight:
-                found_here = packet.search(packet_highlight)
-                if found_here:
-                    if not found_here.groups():
-                        highlight = '<--'
-                    else:
-                        highlight = '<-- %s' % (found_here.groups()[0],)
-                
-            print '%s %s:%d > %s:%d %s %s %s' % (
-                packet.datetime, packet.from_[0], packet.from_[1], packet.to[0], packet.to[1],
-                packet.cseq[0], packet.method_and_status, highlight
-            )
-        print
+        print_dialog(dialog, packet_highlight)
 
 
 if __name__ == '__main__':
@@ -80,7 +84,11 @@ if __name__ == '__main__':
     def my_strptime(timestring):
         return time.mktime(time.strptime(timestring, '%Y-%m-%d %H:%M:%S'))
     def my_timedelta(floatstring):
-        num = [0] + [int(i) for i in floatstring.split('.', 1)]
+        if '.' in floatstring:
+            floatstring = '0%s000000' % (floatstring,) # add enough zeroes so the int value is large enough
+            num = [0, int(floatstring.split('.', 1)[0]), int(floatstring.split('.', 1)[1][0:6])]
+        else:
+            num = [0, int(floatstring), 0]
         if sum(num) == 0:
             raise ValueError('Now I cannot boolean test your value')
         return datetime.timedelta(*num)
@@ -94,8 +102,8 @@ if __name__ == '__main__':
     parser.add_argument('--pcap', '-p', metavar='filter',
             help='pcap filter expression')
 
-    parser.add_argument('--pmatch', '-m', metavar='regex', type=my_regex,
-            help='packet in dialog must match regex')
+    parser.add_argument('--pmatch', '-m', metavar='regex', action='append', type=my_regex,
+            help='packet in dialog must match regex (can be used multiple times)')
     parser.add_argument('--highlight', '-H', metavar='regex', type=my_regex,
             help='highlight first matchgroup in packets')
 
@@ -117,7 +125,7 @@ if __name__ == '__main__':
     else:
         reader = PcapReader(args.files, pcap_filter=args.pcap, min_date=args.mindate, max_date=args.maxdate)
         
-    main(reader, packet_match=args.pmatch, packet_highlight=args.highlight, min_duration=args.mindur, max_duration=args.maxdur)
+    main(reader, packet_matches=args.pmatch, packet_highlight=args.highlight, min_duration=args.mindur, max_duration=args.maxdur)
 
     # Example usage:
     #
