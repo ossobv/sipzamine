faf6c766f87895711521f243d5d65a0f39e0ee1b
Walter Doekes <walter+github@wjd.nu>
2018-10-02 23:11:09 +0200

    build: Add intial travis.yml

---
+++ tests/flake_test.py
@@ -0,0 +1,21 @@
+# vim: set ts=8 sw=4 sts=4 et ai tw=79:
+# sipzamine test case
+# Copyright (C) 2018 Walter Doekes, OSSO B.V.
+
+from unittest import TestCase
+import subprocess
+
+
+class FlakeTestCase(TestCase):
+    def test_flake8(self):
+        subprocess.check_output(['pip', 'install', 'flake8'])
+        proc = subprocess.Popen(
+            "find . -name '*.py' '!' -name 'argparse_1_2_1.py' | "
+            "xargs flake8 --max-line-length=79 --max-complexity=10",
+            shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = proc.communicate()
+        out = out.decode('utf-8', 'replace')
+        err = err.decode('utf-8', 'replace')
+        if proc.wait() != 0:
+            raise AssertionError(
+                'flake8 check failed:\n{}\n{}\n'.format(out, err))
